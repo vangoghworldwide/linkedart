24392a681ff46470c7beb4936be98349a549fdab
Ivo Zandhuis <ivo@zandhuis.nl>
2020-05-20 10:03:08 +0200

    Added turtle examples

---
+++ examples/rdfxml2ttl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# serialize RDF/XML into turtle for the testing and adding examples in turtle
+for rdf_file in rdfxml/*.rdf.xml
+do
+        ttl_file=$(basename -- "$rdf_file")
+        rapper -q -o turtle $rdf_file > ttl/${ttl_file%%.*}.ttl
+done
